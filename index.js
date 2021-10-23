d5d5a4de43540585274ef06e1684aed78a827ddd
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2021-10-23 15:34:49 -0500

    Index

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express');
+require('./db');
+const routesApp = require('./network/routes');
+
+//Inicialización de la app
 const app = express();
+
+app.use(express.json());
+
+//Rutas de la app
+routesApp(app);
 
 app.listen(3000, () => {
     console.log('Corriendo en el puerto 3000');
