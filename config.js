e4864feea7816f91c4e0f0c30307ea40aec5607e
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2021-10-24 16:52:47 -0500

    .env

---
+++ config.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+const config = {
+    userJwtSecret: process.env.AUTH_USER_JWT_SECRET,
+    dbUsername: process.env.DB_USERNAME,
+    dbPassword: process.env.DB_PASSWORD,
+    dbName: process.env.DB_NAME,
+    dbPort: process.env.DB_PORT,
+    dbHost: process.env.DB_HOST
+}
+
+module.exports = config;
