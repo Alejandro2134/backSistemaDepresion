166faf78eaab2579d028ff4be9018953f0480e61
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2022-03-22 10:52:11 -0500

    feat[db]: se agrega función para conectar con la base de datos

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
-const main = () => {
-    startWebApp();
+import logger from '@fnd/external_interfaces/logger';
+
+const Logger = logger(__filename);
+
+const main = async () => {
+    try {
+        createDBConnection();
+        await startWebApp();
+    } catch (err: unknown) {
+        if (err instanceof Error)
+            Logger.error(`ERROR : ${err.message} STACK : ${err.stack}`);
+    }
 };
+
+const createDBConnection = () => {
+    console.log('connection');
+}
 
 const startWebApp = async () => {
     const { Server } = await import('@fnd/web/server');
