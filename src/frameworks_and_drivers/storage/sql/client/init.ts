41558cfe48dde4bb6b46f2153be98aa3da209b05
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2022-04-14 10:43:45 -0500

    feat[heroku]: se agrega variable de entorno para producción

--- src/frameworks_and_drivers/storage/sql/client/init.ts
+++ src/frameworks_and_drivers/storage/sql/client/init.ts
@@ -1,7 +1,8 @@
+import '@fnd/external_interfaces/env';
 import { User } from '../models/user/User';
 
 const dbInit = () => {
-    User.sync({ force: true });
+    User.sync({ force: process.env.ENV == 'development' ? true : false});
 };
 
 export { dbInit };
