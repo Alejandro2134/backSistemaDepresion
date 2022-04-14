43d9fb756dcf5c893523232afd6798057d3c7023
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2022-04-14 17:30:46 -0500

    feat[style]: se aplica estilos

--- src/frameworks_and_drivers/storage/sql/client/init.ts
+++ src/frameworks_and_drivers/storage/sql/client/init.ts
@@ -2,7 +2,7 @@ import '@fnd/external_interfaces/env';
 import { User } from '../models/user/User';
 
 const dbInit = () => {
-    User.sync({ force: process.env.ENV == 'development' ? true : false});
+    User.sync({ force: process.env.ENV == 'development' ? true : false });
 };
 
 export { dbInit };
