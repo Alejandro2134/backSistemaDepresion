69121c898fa8275989f44bf60580e428f4785820
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2022-07-21 20:59:22 -0500

    feat[users]: se agrega validación de correo ya existente en create_one

--- src/domain/modules/user/app_business_rules/users/use_cases/create_one.ts
+++ src/domain/modules/user/app_business_rules/users/use_cases/create_one.ts
@@ -3,16 +3,26 @@ import {
     IUserFDOM,
 } from '@users/enterprise_bussines/entities/user/user_dom';
 import { IOperations } from '@fnd/storage/sql/client/interfaces/ioperations';
+import { ErrorResourceNotFound } from '@common/enterprise_business_rules/dto/errors/resource_not_found';
 
 type Dependencies = {
     usersRepo: IOperations<IUserDOM, IUserFDOM>;
 };
 
+const USER_ALREADY_EXIST = 'A user with this email already exist';
+
 const build = ({ usersRepo }: Dependencies) => {
     const execute = async (item: IUserDOM) => {
-        item.contraseña = null;
-        const createdUser = await usersRepo.create(item);
-        return createdUser;
+        const { email } = item;
+        const user = await usersRepo.getAll({ email: email });
+
+        if(user[0]) {
+            throw new ErrorResourceNotFound(USER_ALREADY_EXIST);
+        } else {
+            item.contraseña = null;
+            const createdUser = await usersRepo.create(item);
+            return createdUser;
+        }
     };
 
     return execute;
