d79f1a46ef04aa2f69ad578a26223fb340f129e5
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2022-07-26 19:30:43 -0500

    fix[errors]: se cambia code al enviar la respuesta

--- src/domain/modules/common/enterprise_business_rules/dto/errors/bad_request.ts
+++ src/domain/modules/common/enterprise_business_rules/dto/errors/bad_request.ts
@@ -1,11 +1,11 @@
 import { BaseError } from '../../bases/base_error';
-import { ErrorsEnum, HTTPCodesEnum } from '../enums/errors_enums';
+import { HTTPCodesEnum } from '../enums/errors_enums';
 
 export class ErrorBadRequest extends BaseError {
     constructor(message: string, metadata?: any) {
         super(
             `${message}`,
-            ErrorsEnum.HTTP_REQUEST,
+            HTTPCodesEnum.BAD_REQUEST,
             HTTPCodesEnum.BAD_REQUEST,
             metadata
         );
