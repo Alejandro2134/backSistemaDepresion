33a07ffc6802a4d4d00f1c23c46193e0621b57d9
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2022-08-09 21:50:54 -0500

    feat[helpers]: se agrega función para verificar el token en el helper token_handler

--- src/frameworks_and_drivers/helpers/token_handler.ts
+++ src/frameworks_and_drivers/helpers/token_handler.ts
@@ -1,14 +1,25 @@
+import { ErrorResourceNotFound } from '@common/enterprise_business_rules/dto/errors/resource_not_found';
 import jwt from 'jsonwebtoken';
 
 const JWT_PRIVATE_KEY = process.env.JWT_PRIVATE_KEY || 'private_key';
 
 export type createTokenFn = (payload: any) => string;
+export type checkTokenFn = (token: string) => string | jwt.JwtPayload;
 
 const createToken: createTokenFn = (payload: any) => {
     const token = jwt.sign(payload, JWT_PRIVATE_KEY, {
-        expiresIn: '7d',
+        expiresIn: '24h',
     });
     return token;
 }
 
-export { createToken };
+const checkToken: checkTokenFn = (token: string) => {
+    try {
+        const payload = jwt.verify(token, JWT_PRIVATE_KEY);
+        return payload;
+    } catch (err) {
+        throw new ErrorResourceNotFound('Invalid Token');
+    }
+}
+
+export { createToken, checkToken };
