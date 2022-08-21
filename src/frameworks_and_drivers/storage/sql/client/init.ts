44601398d2cbf3eddf556f3bd69d7a91d9f66587
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2022-08-21 12:05:06 -0500

    feat[init]: se agrega relación entre sintomas y preguntas

--- src/frameworks_and_drivers/storage/sql/client/init.ts
+++ src/frameworks_and_drivers/storage/sql/client/init.ts
@@ -1,8 +1,27 @@
 import '@fnd/external_interfaces/env';
 import { User } from '../models/user/User';
+import { DepresionType } from '../models/depresion_type/DepresionType';
+import { Diagnosis } from '../models/diagnosis/Diagnosis';
+import { Question } from '../models/question/Question';
+import { Symptom } from '../models/symptom/Symptom';
+import { sequelizeConnection } from '../client/client';
+import { SymptomQuestion } from '../models/symptom_question/SymptomQuestion';
 
 const dbInit = () => {
-    User.sync({ force: process.env.ENV == 'development' ? true : false });
+    createAssociations();
+    User.sync({ force: false });
+    Question.sync({ force: process.env.ENV == 'development' ? true : false });
+    Symptom.sync({ force: process.env.ENV == 'development' ? true : false });
+    DepresionType.sync({ force: process.env.ENV == 'development' ? true : false });
+    Diagnosis.sync({ force: process.env.ENV == 'development' ? true : false });
 };
+
+const createAssociations = () => {
+    Question.belongsToMany(Symptom, { through: SymptomQuestion });
+    Symptom.belongsToMany(Question, { through: SymptomQuestion });
+    //Symptom.belongsToMany(DepresionType, { through: 'depresion_type_symptom' });
+    //DepresionType.belongsToMany(Symptom, { through: 'depresion_type_symptom' });
+    sequelizeConnection.sync();
+}
 
 export { dbInit };
