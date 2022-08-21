0615956e7d400c148cc4aeab3d5d248df69d0ff6
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2022-08-21 11:47:27 -0500

    feat[symptom_question]: se agrega modelo SymptomQuestion

---
+++ src/frameworks_and_drivers/storage/sql/models/symptom_question/SymptomQuestion.ts
@@ -0,0 +1,24 @@
+import { DataTypes, Model } from "sequelize";
+import { sequelizeConnection } from "../../client/client";
+import { ISymptomQuestionDAL } from "./symptom_question_dal";
+
+export class SymptomQuestion extends Model<ISymptomQuestionDAL> implements ISymptomQuestionDAL {
+    id!: string;
+}
+
+SymptomQuestion.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize: sequelizeConnection,
+        underscored: true,
+        modelName: 'symptom_question',
+        freezeTableName: true,
+    }
+)
