a6da6df156920ee9670e1d8f0261d5f066f51696
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2022-09-02 23:37:56 -0500

    feat[question]: se agrega eliminación o agregación de relaciones en caso de uso update

--- src/domain/modules/question/app_business_rules/use_cases/update_one.ts
+++ src/domain/modules/question/app_business_rules/use_cases/update_one.ts
@@ -1,30 +1,36 @@
-import { ErrorBadRequest } from "@common/enterprise_business_rules/dto/errors/bad_request";
-import { ErrorResourceNotFound } from "@common/enterprise_business_rules/dto/errors/resource_not_found";
-import { IOperations } from "@fnd/storage/sql/client/interfaces/ioperations";
-import { IQuestionDOM, IQuestionFDOM, QuestionDOM } from "@questions/enterprise_business/entities/question/question_dom";
+import { ErrorBadRequest } from '@common/enterprise_business_rules/dto/errors/bad_request';
+import { ErrorResourceNotFound } from '@common/enterprise_business_rules/dto/errors/resource_not_found';
+import { IOperations } from '@fnd/storage/sql/client/interfaces/ioperations';
+import {
+    IQuestionDOM,
+    IQuestionFDOM,
+    QuestionDOM,
+} from '@questions/enterprise_business/entities/question/question_dom';
 
 type Dependencies = {
     questionsRepo: IOperations<QuestionDOM, IQuestionFDOM>;
-}
+};
 
 const QUESTION_NOT_FOUND_ERROR = 'question not found';
 const QUESTION_CANT_BE_UPDATED_ERROR = 'question cant be updated';
 
 const build = ({ questionsRepo }: Dependencies) => {
-    const execute = async(id: number, item: IQuestionDOM) => {
-        const question = await questionsRepo.getAll({id: id});
-        if(question[0]) {
+    const execute = async (id: number, item: IQuestionDOM) => {
+        const question = await questionsRepo.getAll({ id: id });
+        if (question[0]) {
             const updateQuestion = question[0].updateQuestion(item);
+
             const result = await questionsRepo.update(id, updateQuestion);
-            if (!result) throw new ErrorBadRequest(QUESTION_CANT_BE_UPDATED_ERROR);
-    
+            if (!result)
+                throw new ErrorBadRequest(QUESTION_CANT_BE_UPDATED_ERROR);
+
             return result;
         } else {
             throw new ErrorResourceNotFound(QUESTION_NOT_FOUND_ERROR);
         }
-    }
+    };
 
     return execute;
-}
+};
 
 export { build };
--- src/frameworks_and_drivers/storage/sql/implementation/question/question_imp.ts
+++ src/frameworks_and_drivers/storage/sql/implementation/question/question_imp.ts
@@ -1,23 +1,31 @@
-import { StorageError } from "@common/enterprise_business_rules/dto/errors/storage_error";
-import { IQuestionFDOM, QuestionDOM } from "domain/modules/question/enterprise_business/entities/question/question_dom";
-import { BaseImplementation } from "../../client/driver/base_sql_impl";
-import { IFilterWrapper, IWrapper } from "../../client/interfaces/iwrapper";
-import { IQuestionFDAL, QuestionDAL } from "../../models/question/question_dal";
+import { StorageError } from '@common/enterprise_business_rules/dto/errors/storage_error';
+import {
+    IQuestionFDOM,
+    QuestionDOM,
+} from 'domain/modules/question/enterprise_business/entities/question/question_dom';
+import { BaseImplementation } from '../../client/driver/base_sql_impl';
+import { IFilterWrapper, IWrapper } from '../../client/interfaces/iwrapper';
+import { IQuestionFDAL, QuestionDAL } from '../../models/question/question_dal';
 import { Question } from '@fnd/storage/sql/models/question/Question';
 import { Op } from 'sequelize';
-import { Symptom } from "../../models/symptom/Symptom";
-import { fromSnakeToCamel } from "@fnd/helpers/from_snake_to_camel";
+import { Symptom } from '../../models/symptom/Symptom';
+import { fromSnakeToCamel } from '@fnd/helpers/from_snake_to_camel';
 
-export class QuestionsSQLImplementation extends BaseImplementation<QuestionDOM, IQuestionFDOM> implements IWrapper<QuestionDOM, QuestionDAL>, IFilterWrapper<IQuestionFDOM, IQuestionFDAL> {
+export class QuestionsSQLImplementation
+    extends BaseImplementation<QuestionDOM, IQuestionFDOM>
+    implements
+        IWrapper<QuestionDOM, QuestionDAL>,
+        IFilterWrapper<IQuestionFDOM, IQuestionFDAL>
+{
     async create(item: QuestionDOM): Promise<QuestionDOM> {
         try {
             const itemDAL = this.fromDomToDal(item);
             const result = await Question.create(itemDAL);
-            
-            if(item.sintomas) {
-                result.addSymptoms(item.sintomas);
+
+            if (item.sintomas) {
+                await result.addSymptoms(item.sintomas);
             }
-            
+
             const resDAL = result.get({ plain: true });
             const resDOM = this.fromDalToDom(resDAL);
             return resDOM;
@@ -30,12 +38,21 @@ export class QuestionsSQLImplementation extends BaseImplementation<QuestionDOM, 
         try {
             const response = await Question.update(this.fromDomToDal(item), {
                 where: {
-                    id: id
+                    id: id,
                 },
                 returning: true,
             });
 
             const result = response[1];
+
+            if (item.sintomas) {
+                await result[0].addSymptoms(item.sintomas);
+            }
+
+            if (item.removerSintomas) {
+                await result[0].removeSymptoms(item.removerSintomas);
+            }
+
             const resDAL = result[0].get({ plain: true });
             const resDOM = resDAL !== null ? this.fromDalToDom(resDAL) : null;
             return resDOM;
@@ -62,15 +79,17 @@ export class QuestionsSQLImplementation extends BaseImplementation<QuestionDOM, 
         try {
             const result = await Question.findAll({
                 where: this.filterDomToDal(filter),
-                include: [{
-                    model: Symptom,
-                    through: {
-                        attributes: []
-                    }
-                }],
+                include: [
+                    {
+                        model: Symptom,
+                        through: {
+                            attributes: [],
+                        },
+                    },
+                ],
             });
 
-            const resDAL = result.map(result => result.get({ plain: true }));
+            const resDAL = result.map((result) => result.get({ plain: true }));
             const resDOM = resDAL.map(this.fromDalToDom);
             return resDOM;
         } catch (error) {
@@ -82,7 +101,7 @@ export class QuestionsSQLImplementation extends BaseImplementation<QuestionDOM, 
         try {
             const result = await Question.findByPk(id);
 
-            if(result) {
+            if (result) {
                 const resDAL = result.get({ plain: true });
                 const resDOM = this.fromDalToDom(resDAL);
                 return resDOM;
@@ -97,7 +116,7 @@ export class QuestionsSQLImplementation extends BaseImplementation<QuestionDOM, 
     async countRegisters(filter: IQuestionFDOM): Promise<number> {
         try {
             return await Question.count({
-                where: this.filterDomToDal(filter)
+                where: this.filterDomToDal(filter),
             });
         } catch (error) {
             throw new StorageError(error);
@@ -108,6 +127,7 @@ export class QuestionsSQLImplementation extends BaseImplementation<QuestionDOM, 
         const entity = new QuestionDAL({
             id: item.id,
             pregunta: item.pregunta,
+            sintomas: item.sintomas,
         });
 
         return entity;
@@ -117,9 +137,10 @@ export class QuestionsSQLImplementation extends BaseImplementation<QuestionDOM, 
         const entity = new QuestionDOM({
             id: item.id,
             pregunta: item.pregunta,
+            sintomas: item.sintomas,
         });
 
-        if(item.symptoms) {
+        if (item.symptoms) {
             entity.symptoms = item.symptoms.map(fromSnakeToCamel);
         }
 
@@ -127,19 +148,17 @@ export class QuestionsSQLImplementation extends BaseImplementation<QuestionDOM, 
     }
 
     filterDomToDal(item: IQuestionFDOM): IQuestionFDAL {
-        const mapFilter: IQuestionFDAL = {
+        const mapFilter: IQuestionFDAL = {};
 
-        }
-
-        for(const key in item) {
-            switch(key) {
+        for (const key in item) {
+            switch (key) {
                 case 'id':
                     mapFilter[key] = item[key];
                     break;
                 case 'pregunta':
                     mapFilter[key] = {
-                        [Op.iLike]: `${item[key]}%`
-                    }
+                        [Op.iLike]: `${item[key]}%`,
+                    };
                     break;
             }
         }
