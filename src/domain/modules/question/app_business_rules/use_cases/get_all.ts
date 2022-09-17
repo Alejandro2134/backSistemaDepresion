3ac57b2c77457ba7a43203762d337c0635e1f1e2
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2022-09-17 00:46:45 -0500

    feat[question]: se agregan estilos de prettier al caso de uso get_all

--- src/domain/modules/question/app_business_rules/use_cases/get_all.ts
+++ src/domain/modules/question/app_business_rules/use_cases/get_all.ts
@@ -1,23 +1,26 @@
-import { IOperations } from "@fnd/storage/sql/client/interfaces/ioperations";
-import { IQuestionFDOM, QuestionDOM } from "@questions/enterprise_business/entities/question/question_dom";
+import { IOperations } from '@fnd/storage/sql/client/interfaces/ioperations';
+import {
+    IQuestionFDOM,
+    QuestionDOM,
+} from '@questions/enterprise_business/entities/question/question_dom';
 
 type Dependencies = {
     questionsRepo: IOperations<QuestionDOM, IQuestionFDOM>;
-}
+};
 
 const build = ({ questionsRepo }: Dependencies) => {
-    const findAll = async(filter: IQuestionFDOM) => {
+    const findAll = async (filter: IQuestionFDOM) => {
         return await questionsRepo.getAll(filter);
-    }
+    };
 
-    const count = async(filter: IQuestionFDOM) => {
+    const count = async (filter: IQuestionFDOM) => {
         return await questionsRepo.countRegisters(filter);
-    }
+    };
 
     return {
         findAll,
-        count
+        count,
     };
-}
+};
 
 export { build };
