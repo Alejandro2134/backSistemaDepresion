c3e337918ab1070104a73cb8ffd854e4aaced64e
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2022-09-29 22:06:09 -0500

    fix[depresion_type]: se pone nombre correcto de la función createOne

--- src/domain/modules/depresion_type/app_business_rules/index.ts
+++ src/domain/modules/depresion_type/app_business_rules/index.ts
@@ -1,6 +1,6 @@
 import { DepresionTypesRepository } from '@depresion_type/interface_adapters/repositories/depresion_type_repository';
 import { DepresionTypesSQLImplementation } from '@fnd/storage/sql/implementation/depresion_type/depresion_type_imp';
-import { build as buildGetOne } from './use_cases/create_one';
+import { build as buildCreateOne } from './use_cases/create_one';
 import { build as buildGetAll } from './use_cases/get_all';
 import { build as buildDeleteOne } from './use_cases/delete_one';
 import { build as buildUpdateOne } from './use_cases/update_one';
@@ -8,7 +8,7 @@ import { build as buildUpdateOne } from './use_cases/update_one';
 const depresionTypesRepo: DepresionTypesRepository =
     new DepresionTypesRepository(new DepresionTypesSQLImplementation());
 
-const createOne = buildGetOne({ depresionTypesRepo });
+const createOne = buildCreateOne({ depresionTypesRepo });
 const getAll = buildGetAll({ depresionTypesRepo });
 const deleteOne = buildDeleteOne({ depresionTypesRepo });
 const updateOne = buildUpdateOne({ depresionTypesRepo });
