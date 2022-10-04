8cf2c51de709d8e61acc34d5e940a611102c86c1
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2022-10-03 21:03:25 -0500

    feat[symptom]: se agrega eliminación de sintoma al eliminar pregunta

--- src/domain/modules/symptom/app_business_rules/index.ts
+++ src/domain/modules/symptom/app_business_rules/index.ts
@@ -4,21 +4,26 @@ import { build as buildCreateOne } from './use_cases/create_one';
 import { build as buildGetAll } from './use_cases/get_all';
 import { build as buildDeleteOne } from './use_cases/delete_one';
 import { build as buiildUpdateOne } from './use_cases/update_one';
+import { QuestionsRepository } from '@questions/interface_adapters/repositories/question_repository';
+import { QuestionsSQLImplementation } from '@fnd/storage/sql/implementation/question/question_imp';
 
+const questionsRepo: QuestionsRepository = new QuestionsRepository(
+    new QuestionsSQLImplementation()
+);
 const symptomsRepo: SymptomsRepository = new SymptomsRepository(
     new SymptomsSQLImplementation()
 );
 
 const createOne = buildCreateOne({ symptomsRepo });
 const getAll = buildGetAll({ symptomsRepo });
-const deleteOne = buildDeleteOne({ symptomsRepo });
+const deleteOne = buildDeleteOne({ symptomsRepo, questionsRepo });
 const updateOne = buiildUpdateOne({ symptomsRepo });
 
 const service = {
     createOne,
     getAll,
     deleteOne,
-    updateOne
+    updateOne,
 };
 
 export default service;
--- src/domain/modules/symptom/app_business_rules/use_cases/delete_one.ts
+++ src/domain/modules/symptom/app_business_rules/use_cases/delete_one.ts
@@ -1,18 +1,38 @@
-import { ErrorResourceNotFound } from "@common/enterprise_business_rules/dto/errors/resource_not_found";
-import { IOperations } from "@fnd/storage/sql/client/interfaces/ioperations";
-import { ISymptomFDOM, SymptomDOM } from "@symptoms/enterprise_business/entities/symptom/symptom_dom";
+import { ErrorResourceNotFound } from '@common/enterprise_business_rules/dto/errors/resource_not_found';
+import { IOperations } from '@fnd/storage/sql/client/interfaces/ioperations';
+import { updateOne } from '@questions/app_business_rules';
+import {
+    IQuestionFDOM,
+    QuestionDOM,
+} from '@questions/enterprise_business/entities/question/question_dom';
+import {
+    ISymptomFDOM,
+    SymptomDOM,
+} from '@symptoms/enterprise_business/entities/symptom/symptom_dom';
 
 type Dependencies = {
+    questionsRepo: IOperations<QuestionDOM, IQuestionFDOM>;
     symptomsRepo: IOperations<SymptomDOM, ISymptomFDOM>;
-}
+};
 
-const build = ({ symptomsRepo }: Dependencies) => {
-    const execute = async(symptomId: number) => {
+const build = ({ symptomsRepo, questionsRepo }: Dependencies) => {
+    const execute = async (symptomId: number) => {
+        const questions = await questionsRepo.getAll({ sintomas: [symptomId] });
+        const symptomsToRemove: any = {
+            removerSintomas: [symptomId],
+        };
+
+        questions.map(async (question) => {
+            if (question.id) {
+                await updateOne(question.id, symptomsToRemove);
+            }
+        });
+
         const res = await symptomsRepo.delete(symptomId);
         if (res == 0) throw new ErrorResourceNotFound(`symptom doesn't exists`);
-    }
+    };
 
     return execute;
-}
+};
 
 export { build };
