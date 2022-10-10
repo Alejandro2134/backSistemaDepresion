eb7d822f9299a6033463b6125c52dcc5193201dc
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2022-10-09 19:52:06 -0500

    feat[datetime]: se agrega cambio de zona hoararia forzosa en función changeFormatDate

--- src/frameworks_and_drivers/external_interfaces/datetime.ts
+++ src/frameworks_and_drivers/external_interfaces/datetime.ts
@@ -2,15 +2,17 @@ import { DateTime } from 'luxon';
 
 export type changeFormatDateFn = (date: Date) => string;
 
+const UTC_TIMEZONE = 'Etc/GMT+5';
+
 export enum DateStringFormatEnum {
     CHANGE_LOG_DATE_FORMAT = 'yyyy/MM/dd t',
 }
 
 const changeFormatDate: changeFormatDateFn = (date: Date) => {
     const resultDate = DateTime.fromJSDate(date);
-    const parse = resultDate.toFormat(
-        DateStringFormatEnum.CHANGE_LOG_DATE_FORMAT
-    );
+    const parse = resultDate
+        .setZone(UTC_TIMEZONE)
+        .toFormat(DateStringFormatEnum.CHANGE_LOG_DATE_FORMAT);
     return parse;
 };
 
